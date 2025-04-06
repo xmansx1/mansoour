49de0764795601a3a42ff1550342c2671c99cdb6
Mansour <xmansx1122@gmail.com>
2025-04-06 15:02:31 +0300

    🛠️ Fix ALLOWED_HOSTS manually to debug 400 error

--- real_estate_platform/settings.py
+++ real_estate_platform/settings.py
@@ -14,7 +14,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # إعدادات الأمان
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-key')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = ['mansoour.onrender.com', '127.0.0.1', 'localhost']
+
 
 
 # التطبيقات المثبتة
